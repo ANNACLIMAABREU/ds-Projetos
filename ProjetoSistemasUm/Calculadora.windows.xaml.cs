2e53865b782f41aaf4c50f1bdbf7bfd6a1e88d82
ANNACLIMAABREU <102303443+ANNACLIMAABREU@users.noreply.github.com>
2022-03-31 10:15:16 -0400

    Add files via upload

---
+++ ProjetoSistemasUm/Calculadora.windows.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjetoSistemasUm
+{
+    /// <summary>
+    /// Lógica interna para Calculadora.xaml
+    /// </summary>
+    public partial class Calculadora : Window
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void btDivisao_Click(object sender, RoutedEventArgs e)
+        {
+            double n1 = Convert.ToDouble(txtN1.Text);
+            double n2 = Convert.ToDouble(txtN2.Text);
+
+            double rest = n1 / n2;
+            txtResult.Text = Convert.ToString(rest);
+        }
+
+        private void btSubtract_Click(object sender, RoutedEventArgs e)
+        {
+            double n1 = Convert.ToDouble(txtN1.Text);
+            double n2 = Convert.ToDouble(txtN2.Text);
+
+            double rest = n1 - n2;
+            txtResult.Text = Convert.ToString(rest);
+        }
+
+        private void btMult_Click(object sender, RoutedEventArgs e)
+        {
+            double n1 = Convert.ToDouble(txtN1.Text);
+            double n2 = Convert.ToDouble(txtN2.Text);
+
+            double rest = n1 * n2;
+            txtResult.Text = Convert.ToString(rest);
+        }
+
+        private void btAdd_Click(object sender, RoutedEventArgs e)
+        {
+            double n1 = Convert.ToDouble(txtN1.Text);
+            double n2 = Convert.ToDouble(txtN2.Text);
+
+            double rest = n1 + n2;
+            txtResult.Text = Convert.ToString(rest);
+        }
+
+        private void btX_Click(object sender, RoutedEventArgs e)
+        {
+            txtN1.Clear();
+            txtN2.Clear();
+            txtResult.Clear();
+        }
+    }
+}
---
+++ ProjetoSistemasUm/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjetoSistemasUm
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btAcessar_Click(object sender, RoutedEventArgs e)
+        {
+           
+            string usuario = txtUser.Text;
+            string senha = txtSenha.Password.ToString();
+
+            if (usuario == "Anna" && senha == "147")
+            {
+                MessageBox.Show("Logado.");
+                Calculadora calculadora = new Calculadora();
+                calculadora.Show();
+                this.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("usuario e/ou senha iválida! Tente novamente.");
+                txtUser.Clear();
+                txtSenha.Clear();
+                txtUser.Focus();
+            }
+
+        }
+    }
+}
